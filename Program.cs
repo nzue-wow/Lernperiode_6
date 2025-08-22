3e539ca95a414853cae59819662cfd842603bb98
Nena <N.zuehlke.inf24@stud.bbbaden.ch>
2025-08-22 11:54:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace RPGLastAirbendergame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public class AvatarTeam()
+        {
+            AvatarTeam Aang = new AvatarTeam();
+            AvatarTeam Katara = new AvatarTeam();
+            AvatarTeam Sokka = new AvatarTeam();
+            AvatarTeam Toph = new AvatarTeam();
+            AvatarTeam Appa = new AvatarTeam();
+            AvatarTeam Momo = new AvatarTeam();
+        }
+
+        public class Thebadpeople()
+        {
+            Thebadpeople FireLordOzai = new Thebadpeople();
+            Thebadpeople AdmiralZhao = new Thebadpeople();
+            Thebadpeople PrincessAzula = new Thebadpeople();
+            Thebadpeople Mai = new Thebadpeople();
+            Thebadpeople TyLee = new Thebadpeople();
+            Thebadpeople CombustionMan = new Thebadpeople();
+            Thebadpeople LongFang = new Thebadpeople();
+            Thebadpeople DaiLi = new Thebadpeople();
+
+        }   
+    }
+}
